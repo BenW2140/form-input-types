2bb6304e5a624c1d530dccf2bc184667e7a37bec
Ben White <bwhite2140@outlook.com>
2020-06-03 14:08:18 -0700

    add color picker form type
    Co-authored by: Payne Fulcher <pfulcher26@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
     const name = $("#name1").val();
     const food = $("#food1").val();
     const music = $("input:radio[name=music]:checked").val();
+    const dob = $("#born").val();
     $("#output").text(name);
   });
+
+  $("form#name").submit(function(event) {
+    event.preventDefault();
+    const input = $("#name1").val();
+    console.log(input)
+    $("#output").text(input);
+  });
+
 });
+ 
+
+ 
