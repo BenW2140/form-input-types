105358f96e0500dc79ac0a3e3e9058ea78691af6
Ben White <bwhite2140@outlook.com>
2020-06-03 13:51:40 -0700

    add user interface scripts for form types used
    Co-Authored by: Payne Fulcher <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $("form#name").submit(function(event) {
     event.preventDefault();
     const name = $("#name1").val();
+    const food = $("#food1").val();
+    const music = $("input:radio[name=music]:checked").val();
     $("#output").text(name);
   });
 });
